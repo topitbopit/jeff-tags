b93af5ce48f686a9de4a9e521326c3e852e9ad0c
hidden-r3d <75956940+hidden-r4d@users.noreply.github.com>
2021-06-20 16:02:18 -0500

    Update Tag db

--- database.lua
+++ database.lua
@@ -1,19 +1,92 @@
 {
-        {"jeffismyname4678","yeah i play among us",6879141802},
-        {"jeffismyname4678_1","yeah i play among us",6879141802},
-        {"jeffismyname4678_2","yeah i play among us",6879141802},
-        {"jeffismyname4678_3","yeah i play among us",6879141802},
-        {"jeffismyname4678_4","yeah i play among us",6879141802},
-        {"jeffismyname4678_5","yeah i play among us",6879141802},
-        {"hoops_morelikepoops","<font size='15'><font color='rgb(255,0,0)'>g</font><font color='rgb(255,128,0)'>a</font><font color='rgb(255,255,0)'>m</font><font color='rgb(128,255,0)'>i</font><font color='rgb(0,255,0)'>n</font><font color='rgb(0,255,128)'>g </font><font color='rgb(0,255,255)'>g</font><font color='rgb(0,128,255)'>o</font><font color='rgb(0,0,255)'>d</font></font><br/><font size='5'><i><font color='#262377'>d</font><font color='#2F2275'>i</font><font color='#372173'>s</font><font color='#3F2071'>c</font><font color='#481F6F'>o</font><font color='#501E6D'>r</font><font color='#581E6B'>d</font><font color='#611D69'>.</font><font color='#691C67'>g</font><font color='#711B65'>g</font><font color='#791A63'>/</font><font color='#821961'>a</font><font color='#8A185F'>3</font><font color='#92185D'>J</font><font color='#9B175B'>E</font><font color='#A31659'>r</font><font color='#AB1557'>9</font><font color='#B41455'>Z</font><font color='#BC1353'>6</font><font color='#C41251'>j</font><font color='#CD124F'>Y</font></i></font>",6892266711},
-        {"appleupmepoophole","aple bana",6879490266},
-        {"gmancopa28alt","LOOL!OL!OL!OLLO!OLOL",6879490266},
-        {"johncenaisa_woman","woman",6879490266},
-        {"boiboiunotgoood","<font color='rgb(255,0,0)'><b><i>Bob Is Hot?</i></b></font>",6879702855},
-        {"RSG_Quackity","Fuck Off",2560019913},
-        {"iaimbotonhoops424","i actually aimbot",6884361709},
-        {"MonkeyFresno","im big stupid",538854000},
-        {"coi0e","i will troll u lol",6863132400},
-        {"yormomaisaguy","<font size='50'><font color='rgb(255,0,0)'>sus</font></font>",6960806146},
-        {"JaclayHollman","Santa",6971795976}
+	{
+		"jeffismyname4678",
+		"yeah i play among us",
+		6879141802
+	},
+	{
+		"jeffismyname4678_1",
+		"yeah i play among us",
+		6879141802
+	},
+	{
+		"jeffismyname4678_2",
+		"yeah i play among us",
+		6879141802
+	},
+	{
+		"jeffismyname4678_3",
+		"yeah i play among us",
+		6879141802
+	},
+	{
+		"jeffismyname4678_4",
+		"yeah i play among us",
+		6879141802
+	},
+	{
+		"jeffismyname4678_5",
+		"yeah i play among us",
+		6879141802
+	},
+	{
+		"hoops_morelikepoops",
+		"<font size='15'><font color='rgb(255,0,0)'>g</font><font color='rgb(255,128,0)'>a</font><font color='rgb(255,255,0)'>m</font><font color='rgb(128,255,0)'>i</font><font color='rgb(0,255,0)'>n</font><font color='rgb(0,255,128)'>g </font><font color='rgb(0,255,255)'>g</font><font color='rgb(0,128,255)'>o</font><font color='rgb(0,0,255)'>d</font></font><br/><font size='5'><i><font color='#262377'>d</font><font color='#2F2275'>i</font><font color='#372173'>s</font><font color='#3F2071'>c</font><font color='#481F6F'>o</font><font color='#501E6D'>r</font><font color='#581E6B'>d</font><font color='#611D69'>.</font><font color='#691C67'>g</font><font color='#711B65'>g</font><font color='#791A63'>/</font><font color='#821961'>a</font><font color='#8A185F'>3</font><font color='#92185D'>J</font><font color='#9B175B'>E</font><font color='#A31659'>r</font><font color='#AB1557'>9</font><font color='#B41455'>Z</font><font color='#BC1353'>6</font><font color='#C41251'>j</font><font color='#CD124F'>Y</font></i></font>",
+		6892266711
+	},
+	{
+		"appleupmepoophole",
+		"aple bana",
+		6879490266
+	},
+	{
+		"gmancopa28alt",
+		"LOOL!OL!OL!OLLO!OLOL",
+		6879490266
+	},
+	{
+		"johncenaisa_woman",
+		"woman",
+		6879490266
+	},
+	{
+		"boiboiunotgoood",
+		"<font color='rgb(255,0,0)'><b><i>Bob Is Hot?</i></b></font>",
+		6879702855
+	},
+	{
+		"RSG_Quackity",
+		"Fuck Off",
+		2560019913
+	},
+	{
+		"iaimbotonhoops424",
+		"i actually aimbot",
+		6884361709
+	},
+	{
+		"MonkeyFresno",
+		"im big stupid",
+		538854000
+	},
+	{
+		"coi0e",
+		"i will troll u lol",
+		6863132400
+	},
+	{
+		"yormomaisaguy",
+		"<font size='50'><font color='rgb(255,0,0)'>sus</font></font>",
+		6960806146
+	},
+	{
+		"JaclayHollman",
+		"Santa",
+		6971795976
+	},
+	{
+		"John Doe",
+		"Hello, World!",
+		nil
+	}
 }
